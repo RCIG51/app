49493c963cedf55fae6add3b1c9c745ba4994bf0
iliya <iliyaghavidel98@gmail.com>
2023-06-18 15:57:12 +0330

    make all page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './+page/home/home.component';
+import { AboutComponent } from './+page/about/about.component';
+import { SignInComponent } from './+page/sign-in/sign-in.component';
+import { SignUpComponent } from './+page/sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    SignInComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
